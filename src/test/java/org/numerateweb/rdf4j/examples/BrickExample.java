4447bece624061e2718561128e6f0a4dce6a58dd
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2023-07-03 14:13:09 +0200

    Add minimal brick schema example.

---
+++ src/test/java/org/numerateweb/rdf4j/examples/BrickExample.java
@@ -0,0 +1,32 @@
+package org.numerateweb.rdf4j.examples;
+
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.sail.memory.MemoryStore;
+import org.numerateweb.rdf4j.NumerateWebInferencer;
+
+import java.io.IOException;
+
+public class BrickExample {
+
+	public static void main(String... args) throws IOException {
+		MemoryStore store = new MemoryStore();
+		NumerateWebInferencer sail = new NumerateWebInferencer(store);
+		Repository repository = new SailRepository(sail);
+		try {
+			try (RepositoryConnection connection = repository.getConnection()) {
+				connection.add(BrickExample.class.getResource("/brick-example.ttl"), RDFFormat.TURTLE);
+
+				connection.getStatements(null,
+						repository.getValueFactory().createIRI("https://brickschema.org/schema/Brick#grossArea"),
+						null).stream().forEach(stmt -> {
+					System.out.println(stmt.getSubject() + " has area " + stmt.getObject());
+				});
+			}
+		} finally {
+			repository.shutDown();
+		}
+	}
+}
